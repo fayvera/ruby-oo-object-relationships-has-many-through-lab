70ea099467a7b62da51d7139cb550c233a84cd23
Fay Vera <fvera.cavalcante@gmail.com>
2021-01-26 17:33:27 -0500

    commit tree 0881dac163a6405a4ad96fa43b6e8f2ff24f4670

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,1 +1,16 @@
+ require 'pry'
+ class Appointment
+    attr_accessor :name, :patient, :doctor
+    @@all = []
+    def initialize(date, patient, doctor)
+        @date = date
+        @patient = patient
+        @doctor = doctor
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
 
+
+ end
--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,24 @@
+require 'pry'
 
+class Genre 
+    attr_accessor :name
+
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def songs
+        Song.all.select {|songs| songs.genre == self}
+    end
+
+    def self.all
+        @@all
+    end
+
+    def artists
+        songs.map(&:artist)
+    end
+
+end
